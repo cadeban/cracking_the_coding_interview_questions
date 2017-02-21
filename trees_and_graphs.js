1c6e0369876b8b17c4bde96edbfbeed9a4063c7d
Cadence Banulis <cbanulis@icloud.com>
2017-02-21 13:54:25 -0800

    add prompts 4.9 BST Sequences to 4.12 Paths with Sum

--- trees_and_graphs.js
+++ trees_and_graphs.js
@@ -255,6 +255,100 @@ Graph.prototype = {
     while (current.parent && current.parent.value < node.value) {
       current = current.parent;
     }
-    return current.parent ? current.parent.value : null;
+    return current.parent ? current.parent.value : null; // review
   }
 }
+
+/*
+  4.7 Build Order
+  You are given a ist of projects and a list of dependencies (which is a list of
+  pairs of projects, where the second project is dependent on the first project).
+  All of a project's dependencies must be built before the project is. Find a
+  build order that will allow the projects to be built. If there is no valid
+  build order, return an error.
+
+  Example
+  Input:
+    projects: a, b, c, d, e, f
+    dependencies: (a, d), (f, b), (b, d), (f, a), (d, c)
+  Output: f, e, a, b, d, c
+ */
+
+
+/*
+  4.8 First Common Ancestor
+  Design an algorithm and wrie code to find the first common ancestor of two
+  nodes in a binary tree. Avoid storing additional nodes in a data structure.
+  NOTE: This is not necessarily a binary search tree.
+ */
+
+ function fCA(root, p, q) {
+   if (root === null) {
+     return false;
+   }
+   if ( !areMyChildren(root, p, q) ) {
+     return;
+   }
+
+
+ }
+
+ function isMyChild(root, node) {
+   if (root === null) {
+     return false;
+   }
+   if (root === node) {
+     return true;
+   }
+
+   return isMyChild(root.left, node) || isMyChild(root.right, node);
+ }
+
+ function areMyChildren(root, node1, node2) {
+   return isMyChild(root, node1) && isMyChild(root, node2);
+ }
+
+/*
+  4.9 BST Sequences
+  A binary search tree was created by traversing through an array from left to
+  right and inserting each element. Given a binary search tree with distinct
+  elements, print all possible arrays that could have led to this tree.
+
+  Example:
+     2
+    / \
+  1     3  -> [[2, 1, 3],[2, 3, 1]]
+ */
+
+
+
+/*
+  4.10 Check Subtree
+  T1 and T2 are two very large binary trees, with T1 much igger than T2. Create
+  an algorithm to determine if T2 is a subtree of T1.
+
+  A tree T2 is a subtree of T1 if there exists a node n in T1 such that the
+  subtree of n is identicial to T2. That is, if you cut off the tree at node n,
+  the two trees would be identical.
+ */
+
+
+
+/*
+  4.11 Random Node
+  You are implementing a inary tree class from scratch which, in addition to
+  insert, find, and delete, has a method getRandomNode() which returns a random
+  node from the tree. All nodes should be equally likely to be chosen. Design
+  and implement an algorithm for getRandomNode, and explain how you would
+  implement the rest of the methods.
+ */
+
+
+/*
+  4.12 Paths with Sum
+  You are given a binary tree in which each node contains an integer value
+  (which might be positive or negative). Design an algorithm to count the number
+  of paths that sum to a given value. The path does not need to start or end at
+  the root or a leaf, but it must go downards (traveling only from parent nodes
+  to child nodes).
+ */
